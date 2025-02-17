638e19ccbf26b97032f2e0116a3a4a97d8966797
Jaytechcrypt <adamuaudu661@gmail.com>
2025-02-17 14:22:10 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Jay', // Your name
-  ownerNumber: '2347070784171', //Your number
+  botName: "Graciano Bot",
+  botOwner: 'Graciano', // Your name
+  ownerNumber: '+234 810 806 7660', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
